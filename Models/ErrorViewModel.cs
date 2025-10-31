49923479aa5789a375a0cd1211eb437f275c5be2
Kpiiale <yuliana.capito@udla.edu.ec>
2025-10-31 12:04:16 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniCore_Comision.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
